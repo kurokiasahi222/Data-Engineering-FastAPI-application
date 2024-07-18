cbcf412a99262a97109b5aaebf7b0118b6aae60b
Asahi Kuroki <kurok011@umn.edu>
2024-07-18 15:30:44 -0500

    wip

--- w2/analyze.py
+++
@@ -1,9 +0,0 @@
-# %%
-import pandas
-
-df = pandas.read_csv("results.csv")
-column_names = df.columns
-df_by_process = df.groupby([column_names[0], column_names[1]]).mean()
-
-print(df_by_process)
-# %%
